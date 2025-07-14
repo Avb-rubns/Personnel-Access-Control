9e2f10ef80e096e0f872a06b6b856eff31d27d48
Rubén Sánchez Rugerio <43524461+Avb-rubns@users.noreply.github.com>
2025-07-14 14:26:49 -0600

    Update AuthService.cs

--- Personnel.Client/Client/Services/AuthService.cs
+++ Personnel.Client/Client/Services/AuthService.cs
@@ -1,32 +1,44 @@
 ﻿namespace Personnel.Client.Client.Services
 {
-    public class AuthService(IProxy proxy, ISnackbar snackbar) : AuthenticationStateProvider
+    public class AuthService(ISnackbar snackbar
+        , HttpClient httpClient) : AuthenticationStateProvider
     {
-        private IProxy Proxy { get; set; } = proxy;
         public ISnackbar Snackbar { get; set; } = snackbar;
+        private readonly HttpClient HttpClient = httpClient;
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             try
             {
-                var response = await Proxy.GetAsync<ResponseData<UserInfoDTO>>("api/v1/auth/me");
 
-                switch (response.StatusCode)
+                var httpResponse = await HttpClient.GetAsync("api/v1/auth/me");
+
+                switch (httpResponse.StatusCode)
                 {
-                    case System.Net.HttpStatusCode.OK:
-                        var claims = new List<Claim> {
-                        new Claim(ClaimTypes.NameIdentifier, response.Data.ID.ToString()),
-                        new Claim(ClaimTypes.Name, response.Data.FirstName),
-                        new Claim(ClaimTypes.Email, response.Data.Email),
-                        new Claim(ClaimTypes.Expiration, response.Data.Expiration.ToString()),
-                        new Claim("Status", response.Data.Status.ToString()),
-                        new Claim(ClaimTypes.Role, response.Data.Role)
+                    case HttpStatusCode.OK:
+                        var content = await httpResponse.Content.ReadAsStringAsync();
+                        var response = JsonSerializer.Deserialize<UserInfoDTO>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+                        if (response is { ID: > 0 })
+                        {
+                            var claims = new List<Claim> {
+                        new Claim(ClaimTypes.NameIdentifier, response.ID.ToString()),
+                        new Claim(ClaimTypes.Name, response.FirstName),
+                        new Claim(ClaimTypes.Email, response.Email),
+                        new Claim(ClaimTypes.Expiration, response.Expiration.ToString()),
+                        new Claim("Status", response.Status.ToString()),
+                        new Claim(ClaimTypes.Role, response.Role)
                         };
-                        var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "cookie"));
-                        return new AuthenticationState(user);
-                    default:
-                        Snackbar.Add("Ocurrió un error inesperado, informe a su jefe", Severity.Error);
+                            var user = new ClaimsPrincipal(new ClaimsIdentity(claims, "cookie"));
+                            return new AuthenticationState(user);
+
+                        }
                         break;
+                    case HttpStatusCode.Unauthorized:
+                        await TryRefreshTokenAsync();
+                        break;
+
                 }
+
             }
             catch (Exception ex)
             {
@@ -36,7 +48,6 @@
         }
         public async Task MarkUserAsLoggedOutAsync()
         {
-            await Proxy.PostAsync<Response>("api/v1/auth/logout");
             NotifyAuthenticationStateChanged(Task.FromResult(
                 new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity()))));
         }
@@ -50,7 +61,8 @@
             bool IsRefresh = false;
             try
             {
-                var response = await Proxy.PostAsync<Response>("api/v1/auth/me");
+
+                var response = await HttpClient.PostAsync("api/v1/auth/refresh", null);
                 switch (response.StatusCode)
                 {
                     case System.Net.HttpStatusCode.OK:
